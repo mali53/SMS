942bf1e12b1c5384ecd5ed1fe9bada7ecfd3cd50
Ravindu Malinga <ravindu_d@epiclanka.net>
2024-01-13 19:50:24 +0530

    social media form completed!

---
+++ School Management System/SocialMedia.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace School_Management_System
+{
+    partial class SocialMedia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SocialMedia));
+            this.pictureBoxsocial = new System.Windows.Forms.PictureBox();
+            this.buttonfacebook = new System.Windows.Forms.Button();
+            this.buttoninsta = new System.Windows.Forms.Button();
+            this.buttonx = new System.Windows.Forms.Button();
+            this.buttonwhatsapp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxsocial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxsocial
+            // 
+            this.pictureBoxsocial.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxsocial.Image")));
+            this.pictureBoxsocial.Location = new System.Drawing.Point(0, 63);
+            this.pictureBoxsocial.Name = "pictureBoxsocial";
+            this.pictureBoxsocial.Size = new System.Drawing.Size(782, 547);
+            this.pictureBoxsocial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxsocial.TabIndex = 0;
+            this.pictureBoxsocial.TabStop = false;
+            // 
+            // buttonfacebook
+            // 
+            this.buttonfacebook.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonfacebook.BackgroundImage")));
+            this.buttonfacebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonfacebook.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonfacebook.FlatAppearance.BorderSize = 0;
+            this.buttonfacebook.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.buttonfacebook.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonfacebook.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonfacebook.Location = new System.Drawing.Point(66, 174);
+            this.buttonfacebook.Name = "buttonfacebook";
+            this.buttonfacebook.Size = new System.Drawing.Size(128, 96);
+            this.buttonfacebook.TabIndex = 18;
+            this.buttonfacebook.UseVisualStyleBackColor = true;
+            this.buttonfacebook.Click += new System.EventHandler(this.buttonfacebook_Click);
+            // 
+            // buttoninsta
+            // 
+            this.buttoninsta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttoninsta.BackgroundImage")));
+            this.buttoninsta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttoninsta.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttoninsta.FlatAppearance.BorderSize = 0;
+            this.buttoninsta.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.buttoninsta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttoninsta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttoninsta.Location = new System.Drawing.Point(250, 174);
+            this.buttoninsta.Name = "buttoninsta";
+            this.buttoninsta.Size = new System.Drawing.Size(128, 96);
+            this.buttoninsta.TabIndex = 19;
+            this.buttoninsta.UseVisualStyleBackColor = true;
+            this.buttoninsta.Click += new System.EventHandler(this.buttoninsta_Click);
+            // 
+            // buttonx
+            // 
+            this.buttonx.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonx.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonx.BackgroundImage")));
+            this.buttonx.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonx.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonx.FlatAppearance.BorderSize = 0;
+            this.buttonx.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.buttonx.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonx.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonx.Location = new System.Drawing.Point(430, 174);
+            this.buttonx.Name = "buttonx";
+            this.buttonx.Size = new System.Drawing.Size(128, 96);
+            this.buttonx.TabIndex = 20;
+            this.buttonx.UseVisualStyleBackColor = false;
+            this.buttonx.Click += new System.EventHandler(this.buttonx_Click);
+            // 
+            // buttonwhatsapp
+            // 
+            this.buttonwhatsapp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonwhatsapp.BackgroundImage")));
+            this.buttonwhatsapp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonwhatsapp.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonwhatsapp.FlatAppearance.BorderSize = 0;
+            this.buttonwhatsapp.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.buttonwhatsapp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonwhatsapp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonwhatsapp.Location = new System.Drawing.Point(602, 174);
+            this.buttonwhatsapp.Name = "buttonwhatsapp";
+            this.buttonwhatsapp.Size = new System.Drawing.Size(128, 96);
+            this.buttonwhatsapp.TabIndex = 21;
+            this.buttonwhatsapp.UseVisualStyleBackColor = true;
+            this.buttonwhatsapp.Click += new System.EventHandler(this.buttonwhatsapp_Click);
+            // 
+            // SocialMedia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(782, 608);
+            this.Controls.Add(this.buttonwhatsapp);
+            this.Controls.Add(this.buttonx);
+            this.Controls.Add(this.buttoninsta);
+            this.Controls.Add(this.buttonfacebook);
+            this.Controls.Add(this.pictureBoxsocial);
+            this.Name = "SocialMedia";
+            this.Text = "Social Media Management";
+            this.Load += new System.EventHandler(this.SocialMedia_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxsocial)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxsocial;
+        private System.Windows.Forms.Button buttonfacebook;
+        private System.Windows.Forms.Button buttoninsta;
+        private System.Windows.Forms.Button buttonx;
+        private System.Windows.Forms.Button buttonwhatsapp;
+    }
+}
---
+++ School Management System/SocialMedia.cs
@@ -0,0 +1,84 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace School_Management_System
+{
+    public partial class SocialMedia : MaterialForm
+    {
+        public SocialMedia()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.BlueGrey700, Primary.BlueGrey900,
+                Primary.BlueGrey500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+        }
+
+        private void SocialMedia_Load(object sender, EventArgs e)
+        {
+            buttonfacebook.Parent = pictureBoxsocial;
+            buttonfacebook.BackColor = Color.Transparent;
+
+            buttoninsta.Parent = pictureBoxsocial;
+            buttoninsta.BackColor = Color.Transparent;
+
+            buttonx.Parent = pictureBoxsocial;
+            buttonx.BackColor = Color.Transparent;
+
+            buttonwhatsapp.Parent = pictureBoxsocial;
+            buttonwhatsapp.BackColor = Color.Transparent;
+        }
+
+        private void buttonfacebook_Click(object sender, EventArgs e)
+        {
+            string websiteUrl = "https://www.facebook.com/";
+
+            // Open the default web browser with the specified URL.
+            Process.Start(websiteUrl);
+        }
+
+        private void buttonx_Click(object sender, EventArgs e)
+        {
+            string websiteUrl = "https://twitter.com/?lang=en";
+
+            // Open the default web browser with the specified URL.
+            Process.Start(websiteUrl);
+        }
+
+        private void buttoninsta_Click(object sender, EventArgs e)
+        {
+            string websiteUrl = "https://www.instagram.com/";
+
+            // Open the default web browser with the specified URL.
+            Process.Start(websiteUrl);
+        }
+
+        private void buttonwhatsapp_Click(object sender, EventArgs e)
+        {
+            string websiteUrl = "https://web.whatsapp.com/";
+
+            // Open the default web browser with the specified URL.
+            Process.Start(websiteUrl);
+        }
+    }
+}
